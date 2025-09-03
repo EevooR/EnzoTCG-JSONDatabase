f18e94b3111529d85dcd900689164fc60e8ea01a
EevooR <32144201+EevooR@users.noreply.github.com>
2025-09-03 19:03:41 -0400

    Worked of JS to pull JSON + Temp JSON fix

--- js/Script.js
+++ js/Script.js
@@ -3,6 +3,25 @@ const cardZoomArea = document.querySelector('#CardZoom');
 const imgCardBig = document.querySelector('#zoomedInCard');
 const interactUI = document.querySelector('#CardDisplay');
 const cardList = document.querySelector('#Cardlist');
+const orFullCards = async () => {
+  const orresponse = await fetch('https://eevoor.github.io/EnzoTCG-JSONDatabase/json/ORfull.json');
+  const ordata = await orresponse.json();
+
+  return ordata;
+};
+
+
+
+window.onload = (event) => {
+  orFullCards();
+  try {
+  ordata.forEach((i) => {
+    console.log(i);
+  });
+} catch (error) {
+  console.log(error);
+}
+};
 
 
 
