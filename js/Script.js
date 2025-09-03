b3c63576f77592ba7ff22bc8a6d43a1273cac3aa
EevooR <32144201+EevooR@users.noreply.github.com>
2025-09-03 19:42:40 -0400

    JSON WORK

--- js/Script.js
+++ js/Script.js
@@ -3,42 +3,43 @@ const cardZoomArea = document.querySelector('#CardZoom');
 const imgCardBig = document.querySelector('#zoomedInCard');
 const interactUI = document.querySelector('#CardDisplay');
 const cardList = document.querySelector('#Cardlist');
+
+// Corrected function declaration
 const orFullCards = async () => {
   const orresponse = await fetch('https://eevoor.github.io/EnzoTCG-JSONDatabase/json/ORfull.json');
   const ordata = await orresponse.json();
-
   return ordata;
 };
 
-
-
-window.onload = (event) => {
-  orFullCards();
+window.onload = async (event) => {
   try {
-  ordata.forEach((i) => {
-    console.log(i);
-  });
-} catch (error) {
-  console.log(error);
-}
+
+    const data = await orFullCards(); // await the async function
+
+    Object.keys(data).forEach((key) => {
+  console.log(key.name);
+});
+
+
+//     data.forEach((i) => {
+//       console.log(i.allmightex.name);
+//     });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-
-
-
 function makebig(url) {
-  try{
-  urla = url;
-  imgCardBig.src = urla;
-  cardZoomArea.style.width = '45%';
-  cardZoomImg.style.display = 'block';
-  // cardZoomImg.style.backgroundSize = 'contain';
-  // cardZoomImg.style.backgroundRepreat = 'no-repeat';
-  cardList.width = "55%";
+  try {
+    imgCardBig.src = url;
+    cardZoomArea.style.width = '45%';
+    cardZoomImg.style.display = 'block';
+    // cardZoomImg.style.backgroundSize = 'contain';
+    // cardZoomImg.style.backgroundRepeat = 'no-repeat';
+    cardList.style.width = "55%";
+  } catch (error) {
+    console.log(error);
+  }
 
-} catch(error) {
-  console.log(error);
-}
-console.log(url);
-
+  console.log(url);
 }
